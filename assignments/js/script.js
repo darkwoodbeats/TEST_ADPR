44ebf8025e5e8ac1a161fb6be81c7aabc28d1527
Caleb Wiedel <caleb@beunanimous.com>
2024-03-26 10:01:12 -0500

    added console log

--- assignments/js/script.js
+++ assignments/js/script.js
@@ -1,4 +1,8 @@
 // Mobile Button Interaction
 const toggleBtn = document.getElementById('mobile-btn');
 const naviList = document.getElementById('navi-list');
+
+console.log(toggleBtn);
+console.log(naviList);
+
 toggleBtn.addEventListener('click', () => { naviList.classList.toggle("active"); });
